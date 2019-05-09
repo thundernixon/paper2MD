c87d188055abd87bb5aad75c4b33fd32240d3a5c
Stephen Nixon <stephennixon@Stephens-MacBook-Pro.local>
2019-05-09 18:30:24 -0400

    first draft of paper conversion script

---
+++ paper2md.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import errno
+import re
+import certifi
+import urllib.request as req
+
+path = sys.argv[1]
+workingDir = os.path.split(path)[0]
+basename = os.path.split(path)[1]
+
+paperDoc = open(path, "r")
+
+contents = paperDoc.readlines()
+
+goodDoc = open(f"{workingDir}/README.md", "w")
+
+# make assets directory
+try:
+    os.mkdir(f"{workingDir}/assets")
+except OSError as exc:
+    if exc.errno != errno.EEXIST:
+        raise
+    pass
+
+
+def downloadImage(url, file):
+    # open image URL and save to local file
+    with req.urlopen(imgUrl, cafile=certifi.where()) as d, open(file, "wb") as opfile:
+        data = d.read()
+        opfile.write(data)
+
+    print(f"Downloaded \n\t{imgUrl}\nas\n\t{file}")
+    print("---------------")
+
+
+counter = 1
+
+# for line in markdown
+for line in contents:
+    # if line follows regex of markdown image, download it and write line with local filepath
+    regexPattern = re.compile(r'(?:!\[(.*?)\]\((.*?)\))')
+    match = regexPattern.findall(line)
+    if match != [] and match[0][1] != '/static/img/pixel.gif':
+        # download the image into $dir/assets
+        imgUrl = match[0][1]
+        filetype = imgUrl.split('.')[-1]
+        filename = f"fig-{counter}.{filetype}"
+        filepath = f"{workingDir}/assets/fig-{filename}"
+
+        downloadImage(imgUrl, filepath)
+        counter = counter + 1
+
+        # update markdown file text with local image path
+
+        goodDoc.write(f"![](assets/{filename})")
+
+        if match[0][1] != '':
+            goodDoc.write("\n"+"match[0][1]")
+
+    # if line isn't an image, just write it as-is
+    else:
+        goodDoc.write(line)
+
+# close the good document to save it
+goodDoc.close()
